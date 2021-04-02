0223202d43a7c3b25d15be00440e0fcbd8c0c26d
SamixDev <70034018+godsamix@users.noreply.github.com>
2021-04-02 18:38:52 +0300

    added history to tokens

--- API/tokens.js
+++ API/tokens.js
@@ -1,40 +1,23 @@
-const apiResponse = require("../helpers/apiResponse");
 const envVar = require('dotenv').config();
 const fetch = require('node-fetch');
 const key = envVar.parsed.API_KEY
 const url = envVar.parsed.Covalent_URL
-const Tokendata = require("../helpers/classes");
-const addr = "0x8c97e535313ed467db86a661f9a79ed6725c2c49"
-
+const {Tokendata, history} = require("../helpers/classes");
 
 
 //async function get and send tokens
 async function sendTokens(address, chain_id = 1, currency) {
     return new Promise((resolve, reject) => {
 
-        fetch(url + `${chain_id}/address/${address}/balances_v2/?`)
+        fetch(url + `${chain_id}/address/${address}/portfolio_v2/?`)
             .then(response => response.json())
             .then(data => {
-                const tokens_data = data.data.items;
-                switch (currency) {
-                    case "usd":
-                        createRespUSD(tokens_data).then(res => {
-                            console.log(res)
-                            resolve(res)
-                        })
-                        break;
-                    case "eur":
-                        // code block
-                        break;
-                    case "eth":
-                        // code block
-                        break;
-                    default:
-                        createRespUSD(tokens_data).then(res => {
-                            console.log(res)
-                            resolve(res)
-                        })
-                }
+                const tokens_data = data.items;
+                createResp(tokens_data).then(res => {
+                    console.log(res)
+                    console.log(res[0])
+                    resolve(res)
+                })
             })
             .catch(error => {
                 resolve(console.log("[]"))
@@ -43,36 +26,28 @@ async function sendTokens(address, chain_id = 1, currency) {
     });
 }
 
-async function createRespUSD(tokens_data) {
+async function createResp(tokens_data) {
     return new Promise((resolve, reject) => {
         let allItems = [];
         tokens_data.forEach(element => {
-            let itemData = new Tokendata(element.contract_name,
+            let arr = []
+            element.holdings.forEach(el =>{
+                let eachHistoricalValue = new history(
+                    el.timestamp,
+                    el.close.balance,
+                    el.close.quote
+                )
+                arr.push(eachHistoricalValue)
+            })
+            let itemData = new Tokendata(
+                element.contract_name,
                 element.contract_ticker_symbol,
                 element.contract_address,
                 element.logo_url,
-                element.balance,
+                element.holdings[0].close.balance,
                 element.contract_decimals,
-                element.quote,
-                element.quote_rate)
-            allItems.push(itemData);
-        })
-        resolve(allItems)
-    });
-}
-
-async function createRespEUR(tokens_data) {
-    return new Promise((resolve, reject) => {
-        let allItems = [];
-        tokens_data.forEach(element => {
-            let itemData = new Tokendata(element.contract_name,
-                element.contract_ticker_symbol,
-                element.contract_address,
-                element.logo_url,
-                element.balance,
-                element.contract_decimals,
-                element.quote,
-                element.quote_rate)
+                element.holdings[0].close.quote,
+                arr)
             allItems.push(itemData);
         })
         resolve(allItems)
--- helpers/classes.js
+++ helpers/classes.js
@@ -1,19 +1,31 @@
-module.exports = function  Tokendata(
-    contract_name, 
-    contract_ticker_symbol, 
-    contract_address, 
+function Tokendata(
+    contract_name,
+    contract_ticker_symbol,
+    contract_address,
     logo_url,
     balance,
     contract_decimals,
     quote,
-    quote_rate)
-    {        
-        this.contract_name = contract_name;
-        this.contract_ticker_symbol = contract_ticker_symbol;
-        this.contract_address = contract_address;
-        this.logo_url = logo_url;
-        this.balance = balance;
-        this.contract_decimals = contract_decimals;
-        this.quote = quote;
-        this.quote_rate = quote_rate;
-    }
+    historycal_value
+) {
+    this.contract_name = contract_name;
+    this.contract_ticker_symbol = contract_ticker_symbol;
+    this.contract_address = contract_address;
+    this.logo_url = logo_url;
+    this.balance = balance;
+    this.contract_decimals = contract_decimals;
+    this.quote = quote;
+    this.historycal_value = historycal_value
+}
+
+function history(
+    timestamp,
+    balance,
+    quote
+    ) {
+    this.timestamp = timestamp;
+    this.balance = balance;
+    this.quote = quote;
+}
+
+module.exports = {Tokendata, history}
