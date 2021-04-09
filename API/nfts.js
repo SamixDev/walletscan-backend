adbf2377236ab6f98c8a71805f16cb596d0ac2ee
SamixDev <70034018+godsamix@users.noreply.github.com>
2021-04-09 20:58:31 +0300

    added send error messages to Get NFTs

--- API/nfts.js
+++ API/nfts.js
@@ -6,6 +6,7 @@ const apiResponse = require("../helpers/apiResponse");
 const key = envVar.parsed.API_KEY
 const url = envVar.parsed.Covalent_URL
 const { NftData } = require("../helpers/classes");
+const e = require('express');
 
 router.get('/nfts', (req, res) => {
     let address = req.query.address;
@@ -23,12 +24,14 @@ router.get('/nfts', (req, res) => {
                 } else {
                     apiResponse.successResponse(res, r)
                 }
-            });
+            }, reason => {
+                apiResponse.ErrorResponse(res, reason)
+              });
 
     }
 });
 
-async function getNfts(address, chain_id){
+async function getNfts(address, chain_id) {
     return new Promise((resolve, reject) => {
         console.time("fetch NFT time from covalent API");
         fetch(url + `${chain_id}/address/${address}/balances_v2/?nft=true`)
@@ -36,11 +39,16 @@ async function getNfts(address, chain_id){
             .then(data => {
                 console.timeEnd("fetch NFT time from covalent API");
                 console.time("filter NFTs");
-                filterNFTs(data).then(res => {
-                    console.timeEnd("filter NFTs");
-                    console.log("--------------------------------------");
-                    resolve(res)
-                });
+                if (data.data && data.error == false) {
+                    filterNFTs(data).then(res => {
+                        console.timeEnd("filter NFTs");
+                        console.log("--------------------------------------");
+                        resolve(res)
+                    });
+                } else {
+                    reject(data.error_message)
+                 //   apiResponse.ErrorResponse(res, data.error_message)
+                }
             }).catch(error => {
                 resolve("")
                 throw error;
@@ -53,8 +61,7 @@ async function filterNFTs(data) {
     return new Promise((resolve, reject) => {
         let nfts = [];
         data.data.items.forEach(el => {
-            if (el.type === "nft"){
-                console.log(el.nft_data[0].external_data)
+            if (el.type === "nft") {
                 let eachNFT = new NftData(
                     el.nft_data[0].external_data.name,
                     el.nft_data[0].external_data.description,
